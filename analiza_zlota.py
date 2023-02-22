024b49ff801d67b0c707c1c938dc213868ac19de
python <gatito@poczta.pl>
2023-02-22 08:37:27 +0100

    wykres, baza danych

--- analiza_zlota.py
+++ analiza_zlota.py
@@ -1,20 +1,68 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-waluta='USD/uncja'
+waluta_usd='USD/uncja'
+waluta_pl='PL/uncja'
+title="kurs złota"
 
-f = open("..\zloto.txt", "r")
-kursy_zlota= []
-kurs_one= {}
-for line in f:
-    elem=line.split('|')
-    print(elem)
-    kursy_zlota.append(elem)
+gold_rate= pd.read_csv('zloto.csv', sep=';', index_col=['data', 'godzina'], header=0) # index_col - co ma być indeksem
+print(gold_rate)
+df = pd.DataFrame(gold_rate)
 
-kursySeries=pd.Series(kursy_zlota)
-print('ilość wpisów: ', kursySeries.size)
-print('najniższy kurs:', kursySeries.min()[0],waluta,'zanotowany dnia: ',kursySeries.min()[1])
-print('najwyższy kurs:',kursySeries.max()[0],waluta, 'zanotowany dnia: ',kursySeries.max()[1])
+# plt.yticks(range(0,5000))
+# filter = df['kolumna'] &gt; 5  || czyli >5
+# print(df)
+# df[['kurs_usd', 'kurs_pl']].plot(colormap='magma')
+# print(df)
+# print(df["kurs_usd","kurs_pl"])
+#  df.plot(figsize=(13,5), subplots=True, title=title, logy=True)
 
-kursySlownik=dict(kursySeries)
-print(kursySlownik)
+df.plot(figsize=(13,5), title=title, subplots=True, linestyle='-.' ,xticks=(range(14)), rot=45, yticks=(range(0,1000,1000)))  # figsize - rozmiar wykresu w calach (szer, wys),
+                                                                                 # xticks - wartości na osi,
+                                                                                 # subplots=True - by nie wszytkie wartości na jednym wykresia a na kilku wykresach
+                                                                                 # rot - kąt o jaki obróci się napis na osi x
+                                                                                 # gdy nie chcemy legendy na wykresie to legend = False
 
+# plt.bar(df['data'],600, color='purple')
+plt.grid()
+plt.title(title)
+plt.xlabel("data")
+plt.ylabel("wartość")
+plt.legend()
+plt.show()
+
+#etykiety pionowe
+
+
+
+
+
+
+#  kursySeries=pd.Series(kursy_zlota)
+#  print('ilość wpisów: ', kursySeries.size)
+#  print('najniższy kurs:', kursySeries.min()[0],waluta,'zanotowany dnia: ',kursySeries.min()[1])
+#  print('najwyższy kurs:',kursySeries.max()[0],waluta, 'zanotowany dnia: ',kursySeries.max()[1])
+
+# kursySlownik=dict(kursySeries)
+# print(kursySlownik)
+
+#  x_points=[]
+#  y_points=[]
+#  for elem in kursySeries:
+#      x_points.append(kursySeries[0])
+#      y_points.append(kursySeries[1])
+
+# dodajemy wykres i umieszczamy punkt startu i spadku
+#  title = "Wykres kursów złota"
+# x=max(x_points)
+# plt.scatter(0, 0, label="Zloto")
+# plt.scatter(0, 0, "oś x")
+# plt.plot(x_points, y_points, marker="+", color="red", label="Kolejne punkty rzutu.")
+
+# plt.plot()
+#  plt.grid()
+#  plt.title(title)
+#  plt.xlabel("data")
+#  plt.ylabel(waluta)
+#  plt.legend()
+# plt.show()
--- kursy_zlota.py
+++ kursy_zlota.py
@@ -1,16 +1,20 @@
 import requests
 import bs4
 import time
+import os
 
 localtime = time.localtime(time.time())
 today = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())  # formatowanie daty do postaci 2023-02-02 14:32:32
+day = today[:10]
+hour = today[11:]
 
 
+print(day)
+print(hour)
 url = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=ZLOTO'
 
 try:
     page = requests.get(url)
-#   type(page)
     page.raise_for_status()
 
 # poniższe polecenie wyświetli nam całą stronę w jednej linii
@@ -19,12 +23,6 @@ try:
 except Exception as exc:
     print("wystąpił błąd: %s" % (exc))
     print(f"sprawdź czy adres strony {url} został wpisany poprawnie")
-
-
-
-
-
-
 
 
 # tworzymy obiekt bs4 by móc przeszukiwać po tagach html
@@ -41,18 +39,30 @@ soup = bs4.BeautifulSoup(page.content, 'html.parser')
 # print(find_all_a[50].getText())                               # wypisze tylko to, co między <a></a> z 51 linku z tablicy
 # print(find_all_a[50])                                         # wypisze całość od <a> do zakończenia </a> włącznie
 
-find_kurs = soup.find('div', {'class': "profilLast"})         # znajdzie tag '<div>' mający jako atrybut class=profilLast
-gold = find_kurs.getText()  # zapisze zawartość tagu w zmiennej 'gold'
+find_gold_rate = soup.find('div', {'class': "profilLast"})         # znajdzie tag '<div>' mający jako atrybut class=profilLast
+gold = find_gold_rate.getText()  # zapisze zawartość tagu w zmiennej 'gold'
 print('obecny kurs złota: ',gold)
-kurs=gold[:1]+ gold[2:5]+'.'+gold[6:8]
+gold_rate = gold[:1] + gold[2:5] + '.' + gold[6:8]
+
+find_pl = soup.find_all('span', {'class':'value'})
+gold_rate_pl = find_pl[4].getText().strip()
+gold_rate_pl = gold_rate_pl[0]+gold_rate_pl[2:]
+gold_rate_pl = gold_rate_pl[:4]+'.'+gold_rate_pl[5:]
+# czy plik istnieje jak nie, tworząc dodaj nagłówek
+print(os.path.isfile('zloto.csv'))
+
+if os.path.isfile('zloto.csv'):
+    with open('zloto.csv', 'a') as plik:                    # open z argumentem 'a' dopisuje do poprzedniego, gdyby było 'w' to usunełoby poprzednią zawartość i wpisało to co teraz itp
+        plik.write(gold_rate + ';' + gold_rate_pl + ';' + day + ';' + hour)  # zampsuje w pliku
+        plik.write('\n')
+else:
+    with open('zloto.csv','a') as plik:  # open z argumentem 'a' dopisuje do poprzedniego, gdyby było 'w' to usunełoby poprzednią zawartość i wpisało to co teraz itp
+        plik.write('kurs_usd' + ';' + 'kurs_pl' + ';' + 'data' + ';' + 'godzina')
+        plik.write('\n')
+        plik.write(gold_rate + ';' + gold_rate_pl + ';' + day + ';' + hour) # zampsuje w pliku
+        plik.write('\n')
 
 
 
-# zapiszemy sobie kurs złota do pliku 'zloto.txt' wraz z datą i godziną
-plik = open('zloto.txt', 'a', encoding="utf-8")     # open z argumentem 'a' dopisuje do poprzedniego, gdyby było 'w' to usunełoby poprzednią zawartość i wpisało to co teraz itp
-plik.write(kurs+";"+today)                          # zampsuje w pliku
-plik.write('\n')
-plik.close()
 
-# print(find_kurs.getText())
 
